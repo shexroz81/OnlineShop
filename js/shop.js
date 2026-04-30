e28ee8e06f7ed37709c51ec0d2746f18f89b48a0
Welorcrav <welorshorts@gmail.com>
2026-04-30 19:37:11 +0500

    Sub headerga stil berildi

--- js/shop.js
+++ js/shop.js
@@ -1,36 +1,30 @@
 const link = 'https://dummyjson.com/products'
 
-fetch(link, {
-  method: 'GET',
-  headers: { 'Content-type': 'Application/json' },
-})
+fetch(link)
   .then((res) => res.json())
   .then((data) => {
-    data.products.forEach((datas) => {
-      if (datas) {
-        console.log(datas)
-        const products = document.getElementById('products')
-        const div = document.createElement('div')
-        div.classList.add('product')
-        div.innerHTML += `
-          <div class="p-image">
-            <img src="${datas.thumbnail}" alt="${datas.title}">
-            <div class="icons">
-              <i class="fa-brands fa-sistrix"></i>
-              <i class="fa-regular fa-heart"></i>
-              <i class="fa-solid fa-retweet"></i>
-            </div>
+    const products = document.getElementById('products')
+
+    data.products.forEach((item) => {
+      const div = document.createElement('div')
+      div.classList.add('product')
+
+      div.innerHTML = `
+        <div class="p-image">
+          <img src="${item.thumbnail}" alt="${item.title}">
+          <div class="icons">
+            <i class="fa-brands fa-sistrix"></i>
+            <i class="fa-regular fa-heart"></i>
+            <i class="fa-solid fa-retweet"></i>
           </div>
-          <div class="p-info">
-<<<<<<< HEAD
-            <h1>${datas.title}</h1>
-=======
-            <h2>${datas.title}</h2>
->>>>>>> bf26a32926086a2fd0444aaa1410988de30b190d
-            <bdi>$${datas.price}</bdi>
-          </div>
-        `
-        products.appendChild(div)
-      }
+        </div>
+        <div class="p-info">
+          <h1>${item.title}</h1>
+          <bdi>$${item.price}</bdi>
+        </div>
+      `
+
+      products.appendChild(div)
     })
   })
+  .catch((err) => console.error(err))
