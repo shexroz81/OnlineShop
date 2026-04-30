2fc118680f7ec6792a4fbaca62580447a7477903
Jahongir <welorshorts@gmail.com>
2026-04-30 13:59:44 +0500

    added shop menu navbar

--- js/shop.js
+++ js/shop.js
@@ -1,55 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const proudctSearch = document.getElementById('product-search')
+const link = 'https://dummyjson.com/products'
 
-  const cats = [
-    { n: 'Best Deals', h: '../pages/BestDeals.html' },
-    { n: 'Bread', h: '#' },
-    { n: 'Deals', h: '#' },
-    { n: 'Juices', h: '#' },
-    { n: 'Organic', h: '#' },
-    { n: 'Organic Vegetables & Fruits', h: '../pages/Organic.html' },
-  ]
-
-  proudctSearch.innerHTML = `
-    <div class="search-part">
-      <div class="category">
-        <div class="toggle-cat">
-          <span class="cat-icon"><i class="fa-solid fa-bars-staggered"></i></span>
-          <span class="toggle-title">All Departments</span>
-          <span class="toggle-icon">
-            <i class="fa-solid fa-angle-up"></i>
-            <i class="fa-solid fa-angle-down"></i>
-          </span>
-        </div>
-        <ul class="categories" style="visibility: hidden;">
-          ${cats.map((c) => `<li class="cat-item"><a href="${c.h}">${c.n}</a></li>`).join('')}
-        </ul>
-      </div>
-
-      <div class="search">
-        <input type="text" placeholder="Search">
-        <button><i class="fa-solid fa-magnifying-glass"></i></button>
-      </div>
-    </div>
-    <div class="sort-part"></div>`
-
-
-
-  const link = 'https://dummyjson.com/products'
-
-  fetch(link, {
-    method: 'GET',
-    headers: { 'Content-type': 'Application/json' },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      data.products.forEach((datas) => {
-        if (datas.category == 'groceries') {
-          console.log(datas)
-          const products = document.getElementById('products')
-          const div = document.createElement('div')
-          div.classList.add('product')
-          div.innerHTML += `
+fetch(link, {
+  method: 'GET',
+  headers: { 'Content-type': 'Application/json' },
+})
+  .then((res) => res.json())
+  .then((data) => {
+    data.products.forEach((datas) => {
+      if (datas.category == 'groceries') {
+        console.log(datas)
+        const products = document.getElementById('products')
+        const div = document.createElement('div')
+        div.classList.add('product')
+        div.innerHTML += `
           <div class="p-image">
             <img src="${datas.thumbnail}" alt="${datas.title}">
             <div class="icons">
@@ -63,9 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
             <bdi>$${datas.price}</bdi>
           </div>
         `
-          products.appendChild(div)
-        }
-      })
+        products.appendChild(div)
+      }
     })
-})
-
+  })
