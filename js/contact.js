c832be5af345baaefbb991ef9a02be504d8032aa
Amirbek <valiyevamirbek77@gmail.com>
2026-05-06 06:56:13 +0500

    orderni tugadi

--- js/contact.js
+++ js/contact.js
@@ -14,4 +14,31 @@ document.addEventListener("DOMContentLoaded", () => {
         dropdownToggle.classList.remove("active");
       });
     }
+ 
+    const form = document.querySelector("form");
+  
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const contactInput = document.getElementById("contact");
+    const messageInput = document.getElementById("message");
+  
+    let dataArray = []; // array
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault(); // page refreshni to'xtatadi
+  
+      const formData = {
+        name: nameInput.value,
+        email: emailInput.value,
+        contact: contactInput.value,
+        message: messageInput.value,
+      };
+  
+      dataArray.push(formData); // objectni arrayga qo‘shamiz
+  
+      console.log(dataArray);
+  
+      // ixtiyoriy: inputlarni tozalash
+      form.reset();
+    });
   });
